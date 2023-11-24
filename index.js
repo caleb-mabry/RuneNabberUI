1354250ebc32c6ddd6af411df72242b377e6f4a4
Caleb Mabry <caleb.mabry.dev@gmail.com>
2023-11-24 01:07:18 -0500

    Rasterized and broke down image

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
-L.tileLayer(
-  "https://cdn.runescape.com/assets/img/external/oldschool/2023/newsposts/2023-07-28/osrs_world_map_july13_2023.png#_ga=2.15430064.729954816.1700803934-711659251.1700065839",
-  {
-    maxZoom: 19,
-  }
-).addTo(map);
+var map = L.map("map").setView([0, 0], 0);
+L.tileLayer("public/{z}/{x}/{y}.png", {
+  maxZoom: 9,
+}).addTo(map);
