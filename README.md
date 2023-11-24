caac9ba2febb2793d68f0a122168fcfe34fafa64
Caleb Mabry <caleb.mabry.dev@gmail.com>
2023-11-24 00:42:42 -0500

    Adding the runescape map

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RuneNabberUI
+
+The UI for RuneNabber
+
+# Tools Used
+
+https://log.jiy.io/2019/06/09/RSMap-How-I-mapped-oldschool-runescape-with-leaflet/
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var map = L.map("map").setView([51.505, -0.09], 13);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+L.tileLayer(
+  "https://cdn.runescape.com/assets/img/external/oldschool/2023/newsposts/2023-07-28/osrs_world_map_july13_2023.png#_ga=2.15430064.729954816.1700803934-711659251.1700065839",
+  {
+    maxZoom: 19,
+  }
+).addTo(map);
